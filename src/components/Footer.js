e66246bc1f329c5734b028be9149368be9b942a9
staggosaurus <mstaggs@umich.edu>
2020-10-05 23:57:04 -0400

    new colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,38 +3,36 @@ import styled from "styled-components";
 import { ProductConsumer } from "../context";
 
 export default function Footer() {
-  return (
-    <ProductConsumer>
-      {value => {
-        return (
-          <FooterWrapper>
-            <div className="container py-4">
-              <div className="row">
-                <div className="col-md-12 d-flex justify-content-around">
-                  <p className="text-capitalize">
-                    &copy; Michael Staggs {new Date().getFullYear()}.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </FooterWrapper>
-        );
-      }}
-    </ProductConsumer>
-  );
+    return (
+        <ProductConsumer>
+            {(value) => {
+                return (
+                    <FooterWrapper>
+                        <div className="container py-4">
+                            <div className="row">
+                                <div className="col-md-12 d-flex justify-content-around">
+                                    <p className="text-capitalize">&copy; Michael Staggs {new Date().getFullYear()}.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </FooterWrapper>
+                );
+            }}
+        </ProductConsumer>
+    );
 }
 
 const FooterWrapper = styled.footer`
-  background: var(--mainOpBlue);
-  color: var(--mainBlack);
-  border-top: 1px solid var(--secondaryBlue);
-  .icon {
-    font-size: 1.5rem;
-    color: var(--mainWhite);
-    transition: var(--mainTranstion);
-  }
-  .icon:hover {
-    color: var(--primaryColor);
-    cursor: pointer;
-  }
+    background: var(--mainWhite);
+    color: var(--mainBlack);
+    border-top: 1px solid var(--secondaryBlue);
+    .icon {
+        font-size: 1.5rem;
+        color: var(--mainWhite);
+        transition: var(--mainTranstion);
+    }
+    .icon:hover {
+        color: var(--primaryColor);
+        cursor: pointer;
+    }
 `;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,61 +5,61 @@ import styled from "styled-components";
 import { ProductConsumer } from "../context";
 import logo from "../images/logo.svg";
 export default function Navbar() {
-  return (
-    <ProductConsumer>
-      {value => {
-        const { cartItems, handleSidebar, handleCart } = value;
-        return (
-          <NavWrapper>
-            <div className="nav-center">
-              <FaBars className="nav-icon" onClick={handleSidebar} />                    
-              <img className="nav-logo" src={logo} alt="React Clothes logo" />
-              <div className="nav-cart">
-                <TiShoppingCart className="nav-icon" onClick={handleCart} />
-                <div className="cart-items">{cartItems}</div>
-              </div>
-            </div>
-          </NavWrapper>
-        );
-      }}
-    </ProductConsumer>
-  );
+    return (
+        <ProductConsumer>
+            {(value) => {
+                const { cartItems, handleSidebar, handleCart } = value;
+                return (
+                    <NavWrapper>
+                        <div className="nav-center">
+                            <FaBars className="nav-icon" onClick={handleSidebar} />
+                            <img className="nav-logo" src={logo} alt="React Clothes logo" />
+                            <div className="nav-cart">
+                                <TiShoppingCart className="nav-icon" onClick={handleCart} />
+                                <div className="cart-items">{cartItems}</div>
+                            </div>
+                        </div>
+                    </NavWrapper>
+                );
+            }}
+        </ProductConsumer>
+    );
 }
 
 const NavWrapper = styled.nav`
-  position: -webkit-sticky;
-  position: sticky;
-  top: 0;
-  width: 100%;
-  padding: 1rem 1.5rem;
-  background: var(--mainOpBlue);
-  border-bottom: 1px solid var(--secondaryBlue);
-  z-index: 2;
-  .nav-center {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    max-width: 1170px;
-    margin: 0 auto;
-  }
-  .nav-icon {
-    font-size: 1.5rem;
-    cursor: pointer;
-  }
-  .nav-cart {
-    position: relative;
-  }
-  .cart-items {
-    background: var(--primaryColor);
-    color: var(--mainWhite);
-    font-size: 0.85rem;
-    position: absolute;
-    top: -8px;
-    right: -8px;
-    padding: 0 5px;
-    border-radius: 50%;
-  }
-  .nav-logo{
-    max-height: 45px;
-  }
+    position: -webkit-sticky;
+    position: sticky;
+    top: 0;
+    width: 100%;
+    padding: 1rem 1.5rem;
+    background: var(--mainWhite);
+    border-bottom: 1px solid var(--secondaryBlue);
+    z-index: 2;
+    .nav-center {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        max-width: 1170px;
+        margin: 0 auto;
+    }
+    .nav-icon {
+        font-size: 1.5rem;
+        cursor: pointer;
+    }
+    .nav-cart {
+        position: relative;
+    }
+    .cart-items {
+        background: var(--primaryColor);
+        color: var(--mainWhite);
+        font-size: 0.85rem;
+        position: absolute;
+        top: -8px;
+        right: -8px;
+        padding: 0 5px;
+        border-radius: 50%;
+    }
+    .nav-logo {
+        max-height: 45px;
+    }
 `;
